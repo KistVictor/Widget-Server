f48e2a4fe1a9af7c263afcc7429d8a86ddae30ff
Victor Kist <victoraugustokist@gmail.com>
2022-05-10 09:37:44 -0300

    inversão de dependencia no registro de dados

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router()
 
@@ -16,13 +18,13 @@ const transport = nodemailer.createTransport({
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
 
-  const feedback = await prisma.feedback.create(({
-    data: {
-      comment,
-      type,
-      screenshot
-    }
-  }))
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository)
+  submitFeedbackUseCase.execute({
+    comment,
+    type,
+    screenshot
+  })
 
   await transport.sendMail({
     from: 'Equipe FeedGet <equipe@feedget.com.br>',
@@ -36,5 +38,5 @@ routes.post('/feedbacks', async (req, res) => {
     ].join('\n')
   })
 
-  return res.status(201).json({ data: feedback })
+  return res.status(201).send();
 })
